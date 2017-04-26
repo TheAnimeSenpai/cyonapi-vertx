f92aa870b07cfa6bcfdaef956780a5a4a511a3f8
Opeyemi Akinnawo <opeyemi.akinnawo@interswitchgroup.com>
2017-04-26 14:43:51 +0100

    created utility test file

---
+++ src/test/java/com/cyonakute/UtilitiesTest.java
@@ -0,0 +1,36 @@
+package com.cyonakute;
+
+import com.cyonakute.models.Announcement;
+import com.cyonakute.utilities.Utilities;
+import junit.framework.TestCase;
+import org.junit.Test;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Opeyemi.Akinnawo on 4/25/2017.
+ */
+public class UtilitiesTest extends TestCase {
+
+    @Test
+    public void ObjectToMapTest() {
+
+        //get the current date...
+        Date testDate = new Date();
+
+        //initialize a new object...
+        //Announcement announcement = new Announcement("Test Sample", "Test sample message", "", testDate);
+
+        //convert it to a map...
+//        Map objMap = Utilities.ObjectToMap(announcement);
+//
+//        //generate the expected map...
+//        Map<String, String> expected = new HashMap<>();
+//        expected.put("title", "Test Sample");
+//        expected.put("message", "Test sample message");
+        //expected.put("linkUrl", "");
+//        expected.put("createdAt", testDate);
+
+    }
+}
